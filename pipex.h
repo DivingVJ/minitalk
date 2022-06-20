df5216364295c132c7047b919f18d48e09d12893
V Vijay Ananth <divingvj@gmail.com>
2022-06-20 23:21:57 +0800

    Initial commit

---
+++ pipex.h
@@ -0,0 +1,20 @@
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <sys/wait.h>
+
+
+/* UTILITY FUNCTONS 
+char	*ft_substr(char const *str, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *str, int c);
+size_t	ft_strlen(const char *str);
+char	*ft_strdup(const char *src); */
+
+#endif
