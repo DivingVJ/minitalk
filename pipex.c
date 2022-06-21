8fef4b0b74c51897843acce78482afea5e2a58bb
V vijay ananth Velayutham <vvelayut@u80z06s01.42kl.edu.my>
2022-06-21 18:07:16 +0800

    21/6 Work

--- pipex.c
+++ pipex.c
@@ -4,11 +4,24 @@
 int main(int argc, char **argv, char **envp)
 {
     int len;
+	int	fid;
+	char	*ptrPath;
+
     if (argc != 2)
     {
         printf("Need 1 arg\n");
         return (-1);
     }
+
+	while (*envp)
+	{
+        ptrPath = strstr(*envp, "PATH");
+//        printf("%s\n", (ptrPath));
+      if (ptrPath) printf("%s\n", (ptrPath+5));
+        *envp++;
+    }
+	fid = access("infile", R_OK );
+	printf("FID = %d\n", fid);
     len = ft_strlen(argv[1]);
     printf("Argv = %s : %d\n", argv[1], len);
     return (0);
